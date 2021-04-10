b1f93c60c50f121d30b2aa122db6ed4f78fb1a20
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-04-10 17:34:49 +0300

    display the day born and Akan name for male

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
-// var firstName = document.querySelector(".firstName");
-var lastName = document.getElementById("lastName");
-// var DOB = document.querySelector("#date");
-// var month = document.querySelector("#month");
-// var year = document.querySelector("#year");
-// var display = document.queryselector("#display");
-// var gender = $("#gender").val();
-var  discover  = document.querySelector("#discover");
-var akanMale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
-
-var akanFemale =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"]
-var days=["Sunday","Monday","Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
-
-var DOB=24
-var month = "August";
-var year = 1997;
-gender="Male"
-var DOB2 = parseInt(DOB);
-var combinedDate = month+" "+DOB2+" "+year;
-var date = new Date(combinedDate)
 
 function findOut(){
+
+// var firstName = document.querySelector(".firstName");
+// var lastName = document.getElementById("lastName");
+//  var DOB = document.querySelector("#date").value;
+// var month = document.querySelector("#month").value;
+// var year = document.querySelector("#year").value;
+// var display = document.queryselector("#display");
+var gender = $("#gender").val();
+const  discover  = document.querySelector("#discover");
+var akanMale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+
+var akanFemale=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"]
+var days=["Sunday","Monday","Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
+
+var DOB=25
+var month = "August";
+var year = 1997;
+ gender="Male"
+var DOB2 = parseInt(DOB);
+var combinedDate = month+" "+DOB2+" "+year;
+var date = new Date(combinedDate);
+
     let dayBorn = date.getDay();
-   if(dayBorn==0 && gender=="Male"){
-    alert("You were born on "+ days[0]+ " and you Akan name is "+akanMale[0])
-   }
+  if(gender=="Male"){
+      if(dayBorn==0){
+          alert("You were born on "+days[0]+" and your Akan name is "+akanMale[0])
+      }
+      if(dayBorn==1){
+        alert("You were born on "+days[1]+" and your Akan name is "+akanMale[1])
+    }
+    if(dayBorn==2){
+        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[2])
+    }
+    if(dayBorn==3){
+        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[3])
+    }
+    if(dayBorn==4){
+        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[4])
+    }
+    if(dayBorn==5){
+        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[5])
+    }
+    
+    if(dayBorn==6){
+        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[6])
+    }
+  }
 
 }
 
