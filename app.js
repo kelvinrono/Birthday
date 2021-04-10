1c7f77d5eda32a3f424e06b5c7d5506bf77b0e71
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-04-10 15:40:54 +0300

    create condition for Kwasi

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var firstName = document.querySelector("#firstName");
-var lastName = document.querySelector("#lastName");
-var date = document.querySelector("#date");
-var month = document.querySelector("#month");
-var year = document.querySelector("#year");
-var display = document.queryselector("#display");
-
+// var firstName = document.querySelector(".firstName");
+var lastName = document.getElementById("lastName");
+// var DOB = document.querySelector("#date");
+// var month = document.querySelector("#month");
+// var year = document.querySelector("#year");
+// var display = document.queryselector("#display");
+// var gender = $("#gender").val();
 var  discover  = document.querySelector("#discover");
-var akanFemale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+var akanMale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
 
-var akanMale =[""]
-var days=["Sunday","Monday","Tuesday", "wednesday", "Thursday", "Friday", "Saturday"]
+var akanFemale =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"]
+var days=["Sunday","Monday","Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
 
-discover.addEventListener('click', findOut);
+var DOB=24
+var month = "August";
+var year = 1997;
+gender="Male"
+var DOB2 = parseInt(DOB);
+var combinedDate = month+" "+DOB2+" "+year;
+var date = new Date(combinedDate)
 
 function findOut(){
-    alert("ready")
+    let dayBorn = date.getDay();
+   if(dayBorn==0 && gender=="Male"){
+    alert("You were born on "+ days[0]+ " and you Akan name is "+akanMale[0])
+   }
+
 }
 
