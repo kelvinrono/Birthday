b05bf0f5106e6219da7fd266e9e8428a857c879c
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-04-10 13:45:49 +0300

    declared the js varibales

---
+++ app.js
@@ -0,0 +1,19 @@
+var firstName = document.querySelector("#firstName");
+var lastName = document.querySelector("#lastName");
+var date = document.querySelector("#date");
+var month = document.querySelector("#month");
+var year = document.querySelector("#year");
+var display = document.queryselector("#display");
+
+var  discover  = document.querySelector("#discover");
+var akanFemale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+
+var akanMale =[""]
+var days=["Sunday","Monday","Tuesday", "wednesday", "Thursday", "Friday", "Saturday"]
+
+discover.addEventListener('click', findOut);
+
+function findOut(){
+    alert("ready")
+}
+
