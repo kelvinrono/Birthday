08ef3d0caf4186c1e0760e5092e4aee3681561eb
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-04-11 23:04:26 +0300

    change  from alerting to outputing in html page display

--- app.js
+++ app.js
@@ -1,77 +1,87 @@
+var discover = document.querySelector("#discover")
+discover.addEventListener('click', function(e){
+    e.preventDefault();
 
-function findOut(){
-
-// var firstName = document.querySelector(".firstName");
-// var lastName = document.getElementById("lastName");
-//  var DOB = document.querySelector("#date").value;
-// var month = document.querySelector("#month").value;
-// var year = document.querySelector("#year").value;
-// var display = document.queryselector("#display");
+var firstName = document.querySelector(".firstName").value;
+var lastName = document.getElementById("lastName").value;
+var DOB = document.querySelector("#date").value;
+var year = document.querySelector("#year").value;
+var show = document.querySelector(".show");
 var gender = $("#gender").val();
+var month = $("#month").val();
 const  discover  = document.querySelector("#discover");
 var akanMale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
 
 var akanFemale=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"]
 var days=["Sunday","Monday","Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
 
-var DOB=25
-var month = "August";
-var year = 1997;
- gender="Female"
+
+
+let fullNames ="Hello, "+ firstName.concat(" "+lastName);
 var DOB2 = parseInt(DOB);
 var combinedDate = month+" "+DOB2+" "+year;
 var date = new Date(combinedDate);
 
+//create element
+const todoDiv = document.createElement('div');
+todoDiv.classList.add('todo');
+//list items
+const todoItem = document.createElement('li')
+// todoItem.innerHTML=fullNames;
+todoItem.classList.add('todo')
+todoDiv.appendChild(todoItem)
+show.appendChild(todoDiv);
+
     let dayBorn = date.getDay();
   if(gender=="Male"){
       if(dayBorn==0){
-          alert("You were born on "+days[0]+" and your Akan name is "+akanMale[0])
+        todoItem.innerHTML=fullNames +", you were born on "+days[0]+" and your Akan name is " +akanMale[0];
       }
       if(dayBorn==1){
-        alert("You were born on "+days[1]+" and your Akan name is "+akanMale[1])
+        todoItem.innerHTML= fullNames+"You were born on "+days[1]+" and your Akan name is "+akanMale[1]
     }
     if(dayBorn==2){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[2])
+        todoItem.innerHTML= fullNames+"You were born on "+days[0]+" and your Akan name is "+akanMale[2]
     }
     if(dayBorn==3){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[3])
+        todoItem.innerHTML= fullNames+"You were born on "+days[0]+" and your Akan name is "+akanMale[3]
     }
     if(dayBorn==4){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[4])
+        todoItem.innerHTML= fullNames+"You were born on "+days[0]+" and your Akan name is "+akanMale[4]
     }
     if(dayBorn==5){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[5])
+        todoItem.innerHTML= fullNames+"You were born on "+days[0]+" and your Akan name is "+akanMale[5]
     }
     
     if(dayBorn==6){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanMale[6])
+        todoItem.innerHTML= fullNames+"You were born on "+days[0]+" and your Akan name is "+akanMale[6]
     }
   }
 
   if(gender=="Female"){
     if(dayBorn==0){
-        alert("You were born on "+days[0]+" and your Akan name is "+akanFemale[0])
+        todoItem.innerHTML= fullNames+"You were born on "+days[0]+" and your Akan name is "+akanFemale[0]
     }
     if(dayBorn==1){
-      alert("You were born on "+days[1]+" and your Akan name is "+akanFemale[1])
+        todoItem.innerHTML= fullNames+"You were born on "+days[1]+" and your Akan name is "+akanFemale[1]
   }
   if(dayBorn==2){
-      alert("You were born on "+days[0]+" and your Akan name is "+akanFemale[2])
+    todoItem.innerHTML= fullNames+"You were born on "+days[2]+" and your Akan name is "+akanFemale[2]
   }
   if(dayBorn==3){
-      alert("You were born on "+days[0]+" and your Akan name is "+akanFemale[3])
+    todoItem.innerHTML= fullNames+"You were born on "+days[3]+" and your Akan name is "+akanFemale[3]
   }
   if(dayBorn==4){
-      alert("You were born on "+days[0]+" and your Akan name is "+akanFale[4])
+    todoItem.innerHTML= fullNames+"You were born on "+days[4]+" and your Akan name is "+akanFemale[4]
   }
   if(dayBorn==5){
-      alert("You were born on "+days[0]+" and your Akan name is "+akanFemale[5])
+    todoItem.innerHTML= fullNames+"You were born on "+days[5]+" and your Akan name is "+akanFemale[5]
   }
   
   if(dayBorn==6){
-      alert("You were born on "+days[0]+" and your Akan name is "+akanFemale[6])
+    todoItem.innerHTML= fullNames+"You were born on "+days[6]+" and your Akan name is "+akanFemale[6]
   }
 }
 
-}
+})
 
